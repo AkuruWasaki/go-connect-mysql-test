81952645e0fb2140a23ca9c6203dcc7699cc85d6
AkuruWasaki <doyouknowakuru@gmail.com>
2021-10-12 11:28:12 +0900

    WIP

--- main.go
+++ main.go
@@ -3,7 +3,15 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
+
+// Open database
+db, err := sql.Open("mysql", "dbname=go_database user=go_test")
+if err != nil {
+  return err
+}
 
 func main() {
 	router := gin.Default()
